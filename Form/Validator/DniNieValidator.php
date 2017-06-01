e382a0d88df733c7919779e42e01d44d8d854efe
Daniel González Cerviño <daniel@desarrolla2.com>
2017-06-01 09:10:12 +0200

    created dni nie validator

---
+++ Form/Validator/DniNieValidator.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Jotelulu package
+ *
+ * Copyright (c) 2017 Adder Global && Devtia
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Desarrolla2\FormBundle\Form\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class DniNieValidator extends ConstraintValidator
+{
+    const DNI_NIE_FORMAT = '/^[XYZ]?([0-9]{7,8})([A-Z])$/i';
+    const LETTER_MAP = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+    /**
+     * @param mixed      $value
+     * @param Constraint $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (strlen($value) != 9 || preg_match(self::DNI_NIE_FORMAT, $value, $matches) !== 1) {
+            return false;
+        }
+        list(, $number, $letter) = $matches;
+
+        return strtoupper($letter) === $this->calculateLetter($number);
+    }
+
+    /**
+     * @param $number
+     *
+     * @return mixed
+     */
+    private function calculateLetter($number)
+    {
+        $map = self::LETTER_MAP;
+
+        return $map[((int)$number) % 23];
+    }
+}
