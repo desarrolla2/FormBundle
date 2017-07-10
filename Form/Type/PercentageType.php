c6181141366cc8650a16e2cce2226149b7c6309a
Daniel González Cerviño <daniel@desarrolla2.com>
2017-07-10 12:32:49 +0200

    [IN-111] - created percenage type

---
+++ Form/Type/PercentageType.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Jotelulu package
+ *
+ * Copyright (c) 2017 Adder Global && Devtia
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Desarrolla2\FormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Range;
+
+class PercentageType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'constraints' => [
+                    new Range(['min' => 0]),
+                ],
+                'scale' => 2,
+                'empty_data' => 0.0,
+                'required' => true,
+                'placeholder' => 'percentage',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ]
+        );
+    }
+
+    public function getParent()
+    {
+        return NumberType::class;
+    }
+}
