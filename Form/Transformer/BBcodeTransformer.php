39d890994bc95a60905a001ccf683232a93d13c3
Daniel González Cerviño <daniel@desarrolla2.com>
2017-08-07 22:54:42 +0200

    [] - WIP - created bbcode textarea editor

---
+++ Form/Transformer/BBcodeTransformer.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the PhpStorm package.
+ *
+ * Copyright (c) Daniel González
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@desarrolla2.com>
+ */
+
+namespace Desarrolla2\FormBundle\Form\Transformer;
+
+use JBBCode\Parser;
+use Symfony\Component\Form\DataTransformerInterface;
+
+class BBcodeTransformer implements DataTransformerInterface
+{
+
+    /** @var Parser */
+    protected $parser;
+
+    /**
+     * @param $parser
+     */
+    public function __construct(Parser $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    /**
+     * @param string $number
+     *
+     * @return string
+     */
+    public function transform($text)
+    {
+        if (null === $text) {
+            return '';
+        }
+
+        return $text;
+    }
+
+    /**
+     * @param string $number
+     *
+     * @return string
+     */
+    public function reverseTransform($text)
+    {
+        $this->parser->parse($text);
+
+        $html = $this->parser->getAsHTML();
+        $url = '@(http(s)?)?(://)?(([a-zA-Z])([-\w]+\.)+([^\s\.]+[^\s]*)+[^,.\s])@';
+
+        return preg_replace($url, '<a href="http$2://$4" target="_blank" rel="nofollow" title="$0">$0</a>', $string);
+    }
+}
---
+++ Form/Type/TextAreaBBCodeType.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the PhpStorm package.
+ *
+ * Copyright (c) Daniel González
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@desarrolla2.com>
+ */
+
+namespace Desarrolla2\FormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class TextAreaBBCodeType extends AbstractType
+{
+    private $transformer;
+
+    public function __construct(DataTransformerInterface $purifierTransformer)
+    {
+        $this->purifierTransformer = $purifierTransformer;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer($this->purifierTransformer);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'constraints' => [
+                    new Length(['min' => 3, 'max' => 5000]),
+                ],
+                'attr' => ['class' => 'bbcode-editor form-control'],
+            ]
+        );
+    }
+
+    public function getParent()
+    {
+        return \Symfony\Component\Form\Extension\Core\Type\TextareaType::class;
+    }
+}
