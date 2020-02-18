60a6be3a4c3390fceb9fd694be9ac120caf370cf
Daniel González <daniel@desarrolla2.com>
2020-02-18 10:24:00 +0100

    HR-594 created twig template transformer

---
+++ Form/Transformer/TwigTemplateTransformer.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Form Bundle package
+ *
+ * Copyright (c) 2017 Daniel González
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@desarrolla2.com>
+ */
+
+namespace Desarrolla2\FormBundle\Form\Transformer;
+
+use ForceUTF8\Encoding;
+use Symfony\Component\Form\DataTransformerInterface;
+
+class TwigTemplateTransformer implements DataTransformerInterface
+{
+    /**
+     * @param mixed $value
+     * @return bool|string
+     */
+    public function reverseTransform($value)
+    {
+        $value = trim(Encoding::fixUTF8($value));
+        $value = htmlentities($value, null, 'utf-8');
+        $value = str_replace('&nbsp;', ' ', $value);
+        $value = urldecode($value);
+        $value = html_entity_decode($value);
+        $value = preg_replace(['#{{[\s]+#', '#[\s]+}}#'], ['{{', '}}'], $value);
+
+        return $value;
+    }
+
+    /**
+     * @param mixed $number
+     * @return mixed|string
+     */
+    public function transform($value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        return $value;
+    }
+}
